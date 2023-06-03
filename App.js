aa94e19759cd4b62ce184c37acc431f2fda34ed9
Jhen <developer@jhen.me>
2023-06-03 11:35:29 +0800

    show logs on UI

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, Pressable, NativeModules} from 'react-native';
+
+function App() {
+  const [logs, setLogs] = useState([]);
+  const trigerTestModule = async () => {
+    setLogs([]);
+    // construct 2000000 items array
+    const array = Array.from(Array(20000).keys());
+    // call native module (run 10 times)
+    for (let i = 0; i < 10; i++) {
+      const t0 = performance.now();
+      await NativeModules.TestModule.test(array);
+      const took = performance.now() - t0;
+      console.log(`Call to test took ${took} milliseconds.`);
+      setLogs(logs => [...logs, `Call to test took ${took} milliseconds.`]);
+    }
+  };
+
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Pressable onPress={trigerTestModule}>
+        <Text>Test</Text>
+      </Pressable>
+      {logs.map((log, index) => (
+        <Text key={index}>{log}</Text>
+      ))}
+    </SafeAreaView>
+  );
+}
+
+export default App;
